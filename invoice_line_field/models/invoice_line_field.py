e27f70953d11a2c4b33a2ba01aac63be39c98361
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-07-30 13:00:33 +0500

    Add files via upload

---
+++ invoice_line_field/models/invoice_line_field.py
@@ -0,0 +1,26 @@
+from odoo import api, fields, models
+
+class AccountMoveModelInherit(models.Model):
+    _inherit = 'account.move'
+
+
+class InvoiceInheritModel(models.Model):
+    _inherit = 'account.move.line'
+
+    quality_id = fields.Many2one("carpet.product.quality")
+    sqf = fields.Float('SQ Feet')
+    discount = fields.Float('Discount')
+
+
+
+class SaleAdvancePayment(models.TransientModel):
+    _inherit = 'sale.advance.payment.inv'
+
+    def _prepare_invoice_values(self, order, name, amount, so_line):
+        res = super()._prepare_invoice_values(order, name, amount, so_line)
+        for rec in res['invoice_line_ids']:
+            rec_list = list(rec)
+            rec_list[2]['quality_id'] = 1
+        hh = 6
+        return res
+
