a334d05fd31c8529449f55cbebf1ac73451fb191
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-08-04 23:09:48 +0500

    Add files via upload

--- carpet_color/models/carpet_roll_and_color.py
+++ carpet_color/models/carpet_roll_and_color.py
@@ -64,7 +64,7 @@ class CarpetColorModel(models.Model):
 
                 total = 0
                 total_foot = 0
-                
+
                 # here we calculate total rolls for line
 
                 total += line.color_0 + line.color_1 + line.color_2 + line.color_3 + line.color_4 + line.color_5 + line.color_6 + line.color_7 + line.color_8 + line.color_9 + line.color_10 + line.color_11 + line.color_12 + line.color_13 + line.color_14 + line.color_15 + line.color_16 + line.color_17 + line.color_18 + line.color_19 + line.color_20 + line.color_21 + line.color_22 + line.color_23 + line.color_24 + line.color_25 + line.color_26 + line.color_27 + line.color_28 + line.color_29 + line.color_30 + line.color_31 + line.color_32 + line.color_33 + line.color_34 + line.color_35 + line.color_36 + line.color_37 + line.color_38 + line.color_39 + line.color_40 + line.color_41 + line.color_42 + line.color_43 + line.color_44 + line.color_45 + line.color_46 + line.color_47 + line.color_48 + line.color_1d + line.color_1l + line.color_1r + line.color_3d + line.color_3l + line.color_4l + line.color_5l + line.color_6l + line.color_6d + line.color_6m + line.color_7l + line.color_10d + line.color_11l + line.color_11r + line.color_12r + line.color_13l + line.color_14d + line.color_17r
