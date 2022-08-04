2672323d3ee60455e2720f171da590aa9ad2ef19
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-08-04 23:14:17 +0500

    Add files via upload

--- carpet_delivery_report/models/models.py
+++ carpet_delivery_report/models/models.py
@@ -8,22 +8,20 @@ class DeliveryPackingModel(models.Model):
         order =  self.env['stock.picking'].browse(docids)
 
         data = []
-        count = 0
+
         for line in order.move_ids_without_package:
 
-            if count != 20:
                 data.append({
                 'design_name': line.design_id.name,
                 'quality_name': line.quality_id.name,
                 'color': line.color,
-                'length': line.length,
-                'width': line.width,
+                'length': line.product_id.carpet_length,
+                'width': line.product_id.carpet_width,
                 'color': line.product_id.carpet_color,
             })
 
 
         return {
             'record1': data,
-            'check': count,
             'order': order
         }
