593ee8cf6c03ae936978a243de7b0a06fef83bf6
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-08-24 13:16:13 +0500

    Add files via upload

--- invoice_line_field/models/invoice_line_field.py
+++ invoice_line_field/models/invoice_line_field.py
@@ -4,6 +4,14 @@ class AccountMoveModelInherit(models.Model):
     _inherit = 'account.move'
 
 
+    @api.model
+    def create(self, vals_list):
+        res = super(AccountMoveModelInherit, self).create(vals_list)
+        order = self.env['sale.order'].search([('name', '=', res['invoice_origin'])])
+        order.invoice_status = 'invoiced'
+        return  res
+
+
 class InvoiceInheritModel(models.Model):
     _inherit = 'account.move.line'
 
